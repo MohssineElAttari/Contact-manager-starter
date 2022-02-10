75a9977ce7da1a4544cd6d13596dc6048b919dfe
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-11 00:48:06 +0100

    Writing our First Test

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,4 +1,17 @@
+package com.programming.techie;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ContactManagerTest {
-  
+
+    @Test
+    public void showldCreateContact(){
+    ContactManager contactManager = new ContactManager();
+    contactManager.addContact("mohssine","elattari","0615722515");
+    assertFalse(contactManager.getAllContacts().isEmpty());
+    assertTrue(!contactManager.getAllContacts().isEmpty());
+    assertEquals(1,contactManager.getAllContacts().size());
+    }
 }
